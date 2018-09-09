2c7c4baa7c83ab2a1764b3e208a43da898182869
17262 <1726251215@qq.com>
2018-09-09 20:08:13 +0800

    Revert "修改swagger扫描包"
    This reverts commit cf0c655853ef8733cfb9754fa90ab0e2223492f4.

--- controller/src/main/java/njust/config/Swagger2Configuration.java
+++ controller/src/main/java/njust/config/Swagger2Configuration.java
@@ -21,7 +21,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @Configuration
 public class Swagger2Configuration {
-    private static final String SWAGGER_SCAN_BASE_PACKAGE = "njust.controller";
+    private static final String SWAGGER_SCAN_BASE_PACKAGE = "njust.service";
 
     @Bean
     public Docket createRestApi() {
